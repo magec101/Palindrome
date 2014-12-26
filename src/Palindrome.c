df13ce4741dd2694b097607e26d3acaecf3342ca
magec101 <magec101@manipalglobal.com>
2014-12-26 09:40:42 +0000

    Submit from IDE @Fri Dec 26 15:10:42 Asia/Kolkata 2014

--- src/Palindrome.c
+++ src/Palindrome.c
@@ -5,32 +5,15 @@
 char palindromeMain(int argc, const char** argv)
 {
 	
-	int iStrlen;
-	char acString[50];
-	int iIndex;
+/* Type your Code here */
 
-    if (argc < 2)
-	    	scanf("%s", acString);
-    else
-	    	strcpy(acString, argv[1]);
-	    	
-    iStrlen = strlen(acString);
-    
-    for(iIndex=0; iIndex < iStrlen/2; iIndex++)
-    {
-		if (acString[iIndex] != acString[iStrlen - iIndex - 1])
-				return 'N';
-    }
-	return 'Y';
 }
 
 
 #ifndef __CXX_TEST_MAIN__
 
 int main(int argc, const char** argv){
-    char result;
-    result = palindromeMain(argc, argv);
-    printf ("%c", toupper(result));
+    printf ("%c", palindromeMain(argc, argv));
     return 0;
 }
 
